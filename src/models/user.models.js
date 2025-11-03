54fc01d20ab8f0b00d404b3a72b32be5842bae09
khilil <khililpatel0704@gmail.com>
2025-11-03 15:38:35 +0530

    add user model with schema definition for name, email, password, and role

---
+++ src/models/user.models.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true,
+    },
+    role: {
+        type: String,
+        enum: ['customer', 'admin'],
+        default: 'customer',
+        require: true
+    }
+}, { timestamps: true })
+
+export const User = mongoose.model('User', userSchema);
